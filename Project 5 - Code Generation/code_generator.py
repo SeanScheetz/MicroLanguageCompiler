8085de60aa5cf3c979b738914be7bdaac53b5fe4
Sean Scheetz <xeronatesbad@gmail.com>
2016-04-18 07:17:49 -0400

    recursive expression work -  is amazing!

--- Project 5 - Code Generation/code_generator.py
+++ Project 5 - Code Generation/code_generator.py
@@ -28,9 +28,6 @@ def start(s, outfile):
 	outfile.write("\t.data\n") #start of the data section
 	convert_symbol_table(outfile, s) #write symbol table to .data section
 	outfile.write("prompt_int:\t.asciiz\t\"Enter an int to store in a variable: \"\n") #user instruction
-	outfile.write("sum_stack:\t.word\t0:20\n") #write partial sum to this stack during expression recursion - max 20 recursions
-	outfile.write("var_stack:\t.word\t0:20\n") #write variables waiting to be added/subtracted to this stack when recurse - e.g. 4 + (3 + 2) would right 4 to this stack
-	outfile.write("address_counter:\t.word\t0\n") #keeps track of where the stack address is for the current recursion
 	outfile.write("\n")
 
 	outfile.write("\t.text\n")
@@ -43,19 +40,19 @@ def read_ids(node, outfile):
 	outfile.write("# Reading values for an <id_list>.\n")
 	for child in node.children:
 		#prompt user for int
-		outfile.write("li\t$v0, 4\n") #4 is the syscall to print a str
-		outfile.write("la\t$a0, prompt_int\n") #loads starting address of prompt string into $a0 (arg 0)
+		outfile.write("li\t\t$v0, 4\n") #4 is the syscall to print a str
+		outfile.write("la\t\t$a0, prompt_int\n") #loads starting address of prompt string into $a0 (arg 0)
 		outfile.write("syscall\n")
 
 		#read int
-		outfile.write("li\t$v0, 5\n") #5 is the syscall to read an int
+		outfile.write("li\t\t$v0, 5\n") #5 is the syscall to read an int
 		outfile.write("syscall\n") #after syscall, $v0 holds the int read in
-		outfile.write("sw\t$v0, " + child.val + "\n\n") #store val in $v0 in the memory allocated to the variable
+		outfile.write("sw\t\t$v0, " + child.val + "\n\n") #store val in $v0 in the memory allocated to the variable
 
 def write_ids(node, outfile):
 	outfile.write("# Writing values of an <expr_list>.\n")
 	for child in node.children:
-		outfile.write("li\t$v0, 1\n") #1 is the syscall to print an int
+		outfile.write("li\t\t$v0, 1\n") #1 is the syscall to print an int
 		store_expression_result(child, outfile) #stores result of the child expression in $t0
 		outfile.write("move\t$a0, $t0\n") #move the expression result (int to be printed) that is in $t0 into $a0 (argument 0)
 		outfile.write("syscall\n")
@@ -64,37 +61,43 @@ def assign(node, outfile):
 	ident = node.children[0].val #children[0] will always be <ident>
 	outfile.write("# assign value to " + ident + ".\n")
 	store_expression_result(node.children[1], outfile) #children[1] will always be <expression>, stored in $t0
-	outfile.write("sw\t$t0, " + ident + "\n\n")
+	outfile.write("sw\t\t$t0, " + ident + "\n\n")
 
 #this is infix from your augmented grammar
 #$t0 will accumulate the value (hold the result)
-#$t1 will hold the number waiting to be added/subtracted
 def store_expression_result(node, outfile):
-	outfile.write("li\t$t0, 0\n") #$t0 is going to accumulate the value
-	add = True #add the first number
+	outfile.write("li\t\t$t0, 0\n") #$t0 is going to accumulate the value
+	plus = True #add the first number
 	for child in node.children:
 		if child.label == "PRIMARY":
 			for primary_child in child.children:
 				if primary_child.label == "IDENT":
-					outfile.write("lw\t$t1, " + primary_child.val + "\n")
-					if add:
-						outfile.write("add\t$t0, $t0, $t1" + "\n")
-					else: #subtract
-						outfile.write("sub\t$t0, $t0, $t1" + "\n")
+					outfile.write("lw\t\t$t1, " + primary_child.val + "\n")
+					if plus:
+						outfile.write("add\t\t$t0, $t0, $t1\n")
+					else: #minus
+						outfile.write("sub\t\t$t0, $t0, $t1\n")
 				if primary_child.label == "INTLIT":
-					outfile.write("li\t$t1, " + primary_child.val + "\n")
-					if add:
-						outfile.write("add\t$t0, $t0, $t1" + "\n")
-					else: #subtract
-						outfile.write("sub\t$t0, $t0, $t1" + "\n")
-				if primary_child.label == "EXPRESSION": #problem with current method is $t8 and $t9 will get over written if more than 1 recursive call
-					#save values from current expression call in different variable
-					outfile.write("move\t$t0, $t8\n") #$t8 now holds the current sum
-					outfile.write("move\t$t1, $t9\n") #$t9 now holds the number waiting to be added 2 (e.g. if we are doing 2 + (1 + 3) then $t1 had 2 in it
+					outfile.write("li\t\t$t1, " + primary_child.val + "\n")
+					if plus:
+						outfile.write("add\t\t$t0, $t0, $t1\n")
+					else: #minus
+						outfile.write("sub\t\t$t0, $t0, $t1\n")
+				# ( 1 + (2 + (3 + 4) ) )
+				if primary_child.label == "EXPRESSION":
+					#write the current sum and number waiting to be added to the stack pointer $sp
+					outfile.write("addi\t$sp, $sp, -4\n") #-4 because we are going to push 1 word onto the stack
+					outfile.write("sw\t\t$t0, 0($sp)\n") # store the current sum
 					#make recursive expression call
 					store_expression_result(primary_child, outfile)
-					#reset values and add the value gotten from the recursive call to the value total
+					#restore sum from the stack and increment the stack point
+					outfile.write("lw\t\t$t2, 0($sp)\n")
+					outfile.write("addi\t$sp, $sp, 4\n")
+					if plus:
+						outfile.write("add\t\t$t0, $t0, $t2\n") #add old sum to the result of the recursion expression
+					else: #subtract
+						outfile.write("sub\t\t$t0, $t2, $t0\n") #sub recursion result from old sum
 		if child.label == "PLUS":
-			add = True
+			plus = True
 		if child.label == "MINUS":
-			add = False
+			plus = False
--- Project 5 - Code Generation/out.asm
+++ Project 5 - Code Generation/out.asm
@@ -1,42 +1,44 @@
 	.data
 x:	.word	0
-z:	.word	0
-w:	.word	0
 y:	.word	0
 prompt_int:	.asciiz	"Enter an int to store in a variable: "
-sum_stack:	.word	0:20
-var_stack:	.word	0:20
-address_counter:	.word	0
 
 	.text
 main:
-# assign value to w.
-li	$t0, 0
-li	$t1, 1
-add	$t0, $t0, $t1
-sw	$t0, w
+# assign value to y.
+li		$t0, 0
+li		$t1, 12
+add		$t0, $t0, $t1
+sw		$t0, y
 
 # assign value to x.
-li	$t0, 0
-li	$t1, 3
-add	$t0, $t0, $t1
-sw	$t0, x
+li		$t0, 0
+li		$t1, 1
+add		$t0, $t0, $t1
+addi	$sp, $sp, -4
+sw		$t0, 0($sp)
+li		$t0, 0
+li		$t1, 2
+add		$t0, $t0, $t1
+addi	$sp, $sp, -4
+sw		$t0, 0($sp)
+li		$t0, 0
+li		$t1, 3
+add		$t0, $t0, $t1
+lw		$t1, y
+add		$t0, $t0, $t1
+lw		$t2, 0($sp)
+addi	$sp, $sp, 4
+add		$t0, $t0, $t2
+lw		$t2, 0($sp)
+addi	$sp, $sp, 4
+add		$t0, $t0, $t2
+sw		$t0, x
 
-# assign value to y.
-li	$t0, 0
-li	$t1, 10
-add	$t0, $t0, $t1
-sw	$t0, y
-
-# assign value to z.
-li	$t0, 0
-lw	$t1, x
-add	$t0, $t0, $t1
-li	$t1, 5
-sub	$t0, $t0, $t1
-lw	$t1, y
-add	$t0, $t0, $t1
-lw	$t1, y
-add	$t0, $t0, $t1
-sw	$t0, z
-
+# Writing values of an <expr_list>.
+li		$v0, 1
+li		$t0, 0
+lw		$t1, x
+add		$t0, $t0, $t1
+move	$a0, $t0
+syscall
