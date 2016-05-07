bd2234efc21cc5ffa345926d36a19791f47136d8
Sean Scheetz <xerunix@gmail.com>
2016-05-07 17:41:19 -0400

    updated tokens to allow special characters in strings

--- part6/out.asm
+++ part6/out.asm
@@ -1,4 +1,26 @@
 	.data
 prompt_int:	.asciiz	"Enter an int to store in a variable: "
 x:	.word	0
-y:	.word	0
+
+	.text
+# assign value to x.
+li	$t0, 0
+li	$t1, 0
+li	$t2, 12
+add	$t1, $t1, $t2
+add	$t0, $t0, $t1
+sw		$t0, x
+
+# Writing values of an <expr_list>.
+# Writing an integer expression
+li		$v0, 1
+li	$t0, 0
+li	$t1, 0
+lw	$t2, x
+add	$t1, $t1, $t2
+add	$t0, $t0, $t1
+move	$a0, $t0
+syscall
+addi	$a0, $zero, 0xA
+addi	$v0, $zero, 0xB
+syscall
